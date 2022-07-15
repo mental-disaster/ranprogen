3f21cb8bb4d095c4c76d9c5c4b1de2388e977372
mental_disaster <ghgh9244@gmail.com>
2022-07-15 11:01:22 +0900

    init commit

---
+++ src/main/java/com/example/ranprogen/RanprogenApplication.java
@@ -0,0 +1,13 @@
+package com.example.ranprogen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanprogenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RanprogenApplication.class, args);
+    }
+
+}
